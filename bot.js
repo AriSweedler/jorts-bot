0499b1af321bdadaac623bb0527e1ca8af4fbf67
Ari Sweedler <ari@sweedler.com>
2019-03-16 17:40:28 -0700

    Also log the currently known sender_ids

--- bot.js
+++ bot.js
@@ -5,37 +5,45 @@ const people_listeners = [
   require('./listener/alan')
 ];
 
-
 /* callback to respond to all POST requests (GroupMe messages send POSTS
- * requests to the bot */
+ * requests to the bot) */
 function respond() {
   /* get the message data from the raw HTTP request */
   var message = JSON.parse(this.req.chunks[0]);
+
+  /* If the message was sent by the bot, don't do anything (don't even log it) */
+  if (String(message.sender_id) == process.env.MY_ID) {
+    return;
+  }
+
+  /* Log the name and sender_id of the message */
   console.log(`We have a message from ${message.name}. (sender ID = ${message.sender_id}): ${message.text}`);
   if (!(message.name in known_sender_ids)) {
     known_sender_ids[message.name] = message.sender_id;
   }
 
-  /* don't respond to the bot's message */
-  if (String(message.sender_id) == process.env.MY_ID) {
-    return;
-  }
-
-  /* Send the message through each module's 'listener' callback */
+  /* Send the message through each "require"d module's "listener" callback */
   people_listeners.map(person => person.listener(message));
 
-  /* We don't respond to the request at all, our listener callbacks will send a POST request */
+  /* We don't send an HTTP response to the request at all, as our listener callbacks will handle responses. */
   this.res.end();
 }
 
 /* when GET requested, describe what you do */
+const known_ids = {
+  ...process.env,
+  "BOT_ID": "redacted"
+}
 function describe() {
   console.log(`Someone asked me to describe myself.`);
   this.res.writeHead(200);
   this.res.end(`
 Our callbacks: ${people_listeners.map(person => person.name).join(', ')}.
-known sender ids:
+newly learned sender ids:
 ${JSON.stringify(known_sender_ids, null, '\t')}
+
+already known sender ids:
+${JSON.stringify(known_ids, null, '\t')}
   `);
 }
 
